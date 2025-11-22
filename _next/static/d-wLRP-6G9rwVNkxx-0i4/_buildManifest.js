eb899a05aec35de64944cadb979ecc543434a162
aniket-wss <aniket@wharfstreetstudios.com>
2025-11-22 14:00:01 +0530

    Deploy to gh-pages

---
+++ _next/static/d-wLRP-6G9rwVNkxx-0i4/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Epiko-Hub//_next/:path+",
+        "destination": "/Epiko-Hub/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
